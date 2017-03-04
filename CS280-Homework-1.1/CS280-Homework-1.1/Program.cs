d6e4964022841e96c3101b899a31b836503fbbe6
due2401 <hibik610@gmail.com>
2017-03-04 13:02:19 +0800

    初始化專案

---
+++ CS280-Homework-1.1/CS280-Homework-1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS280_Homework_1._1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
